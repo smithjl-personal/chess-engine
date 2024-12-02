5d566a453530c6f9fa2e13b8a13fe8c24e18b255
master14270 <jessie.smith14270@gmail.com>
2024-12-02 15:33:32 -0500

    refactor: Creates `enum` to differentiate piece types. Extracts and stores whose turn it is from the FEN.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use std::array;
 const BOARD_SIZE: usize = 8;
 
+#[derive(PartialEq)]
+enum PieceType {
+    King,
+    Queen,
+    Rook,
+    Bishop,
+    Knight,
+    Pawn,
+    None,
+}
+
 // Struct for each piece on the board
 struct Piece {
-    empty: bool,
+    piece_type: PieceType,
     white: bool,
     short_name: String,
 }
@@ -12,7 +23,7 @@ struct Piece {
 impl Default for Piece {
     fn default() -> Self {
         Piece {
-            empty: true,
+            piece_type: PieceType::None,
             white: false,
             short_name: String::from(""),
         }
@@ -22,9 +33,11 @@ impl Default for Piece {
 // Struct for the chessboard, which is a 2D array of Pieces
 struct Game {
     board: [[Piece; BOARD_SIZE]; BOARD_SIZE],
-
-    // TODO: Store more data here.
-    // white_to_move: bool,
+    white_to_move: bool,
+    // TODO: Store castling rights?
+    // TODO: Store 'en-pessant' state?
+    // TODO: Store half-moves since last pawn capture or advance.
+    // TODO: Store full-move count (increment after black moves).
 }
 
 // Implement the Default trait for Board
@@ -33,7 +46,7 @@ impl Default for Game {
         // Use from_fn to initialize the 2D array
         Game {
             board: array::from_fn(|_i| array::from_fn(|_j| Piece::default())),
-            // white_to_move: true,
+            white_to_move: true,
         }
     }
 }
@@ -52,7 +65,7 @@ fn print_board(game: &Game) {
     // Here we can print the board or something relevant to it
     for row in game.board.iter() {
         for piece in row.iter() {
-            if piece.empty {
+            if piece.piece_type == PieceType::None {
                 print!(" E "); // E for empty piece
             } else {
                 print!(" {} ", piece.short_name); // Print the short name of the piece
@@ -67,7 +80,7 @@ fn import_fen(fen: &str) -> Game {
     let trimmed_full_fen = fen.trim();
 
     // Split by first space to separate board position from the rest of the FEN components.
-    let mut parts = trimmed_full_fen.splitn(2, ' ');
+    let mut parts = trimmed_full_fen.split(' ');
     let board_str = parts.next().expect("No board position found in FEN.");
 
     // Prepare to populate our board.
@@ -104,7 +117,7 @@ fn import_fen(fen: &str) -> Game {
 
             // Place the piece on the board.
             let piece = &mut game.board[y_pos][x_pos];
-            piece.empty = false;
+            piece.piece_type = piece_char_to_type(&piece_letter);
             piece.white = c.is_ascii_uppercase();
             piece.short_name = c.to_string();
 
@@ -124,5 +137,44 @@ fn import_fen(fen: &str) -> Game {
         panic!("Board must have exactly 8 rows. We parsed: {}", y_pos);
     }
 
+    // Store whose turn it is to move.
+    let whose_turn = parts.next().expect("Not sure whose turn it is.");
+    if whose_turn.to_ascii_lowercase() == "w" {
+        game.white_to_move = true;
+    } else if whose_turn.to_ascii_lowercase() == "b" {
+        game.white_to_move = false;
+    } else {
+        panic!("Unexpected character for whose turn it is: {}. Should be 'w' or 'b'.", whose_turn);
+    }
+
     return game;
+}
+
+/*
+    This code assumes the piece name is valid. Consider consolidating this block somehow with the import.
+*/
+fn piece_char_to_type(c: &char) -> PieceType {
+
+    // TODO: Can this be cleaned up with a `match` statement?
+    if *c == 'k' {
+        return PieceType::King;
+    }
+    else if *c == 'q' {
+        return PieceType::Queen;
+    }
+    else if *c == 'r' {
+        return PieceType::Rook;
+    }
+    else if *c == 'b' {
+        return PieceType::Bishop;
+    }
+    else if *c == 'n' {
+        return PieceType::Knight;
+    }
+    else if *c == 'p' {
+        return PieceType::Pawn;
+    }
+    else {
+        panic!("Unknown piece name given, {}", c);
+    }
 }
