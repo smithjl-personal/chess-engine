29ddd7308d4c817d2c771c4e0e610a07f0e68d8c
master14270 <jessie.smith14270@gmail.com>
2024-12-03 18:59:31 -0500

    feat: Adds structs for coordinates and moves. Also created print functions for both of those structs. Started on function to find all legal moves in position.

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,45 @@ enum PieceType {
     None,
 }
 
+#[derive(Debug)]
+struct Coord {
+    x: usize,
+    y: usize,
+}
+
+impl std::fmt::Display for Coord {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let file = (b'a' + self.x as u8) as char;
+        let rank = BOARD_SIZE - self.y;
+        write!(f, "{}{}", file, rank)
+    }
+}
+
+#[derive(Debug)]
+struct Move {
+    from: Coord,
+    to: Coord,
+    // TODO: Consider storing if move is a capture, and if move is a check. Will help find good moves.
+}
+
+impl std::fmt::Display for Move {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}{}", self.from, self.to)
+    }
+}
+
 // Struct for each piece on the board
 struct Piece {
     piece_type: PieceType,
     white: bool,
     short_name: String,
+
+    // To see our legal moves, we track the position of each actual piece.
+    x: usize,
+    y: usize,
+
+    // Important for castling, and pawns.
+    has_moved: bool,
 }
 
 // Implement the Default trait for Piece
@@ -26,7 +60,72 @@ impl Default for Piece {
             piece_type: PieceType::None,
             white: false,
             short_name: String::from(""),
+
+            x: 0,
+            y: 0,
+
+            has_moved: false,
         }
+    }
+}
+
+impl Piece {
+    fn get_legal_moves(&self, game: &Game) -> Vec<Move> {
+        let mut moves: Vec<Move> = vec![];
+
+        // We can only move if it is our turn.
+        if self.white != game.white_to_move {
+            return moves;
+        }
+
+        // TODO: Is there a way to encapsulate this logic insite the PieceType enum somehow?
+        match self.piece_type {
+            PieceType::King => {
+                /*
+                 * King can move one tile in any direction. `..=` means range inclusive.
+                 * This section handles normal parts, not castling.
+                 */
+                for y  in (self.y.saturating_sub(1))..=(self.y + 1).min(BOARD_SIZE - 1) {
+                    for x in (self.x.saturating_sub(1))..=(self.x + 1).min(BOARD_SIZE - 1) {
+                        let target_piece: &Piece = &game.board[y as usize][x as usize];
+
+                        // We cannot move on a piece that is our own color.
+                        if target_piece.piece_type != PieceType::None && target_piece.white == self.white {
+                            continue;
+                        }
+
+                        // We cannot make a move that would put us in check.
+                        // TODO: Implement this?
+
+                        // Otherwise, move is legal.
+                        let from_tile: Coord = Coord {
+                            x: self.x,
+                            y: self.y,
+                        };
+                        let to_tile: Coord = Coord {
+                            x: x,
+                            y: y,
+                        };
+                        moves.push(Move {
+                            from: from_tile,
+                            to: to_tile,
+                        });
+                    }
+                }
+
+                // TODO: Check for castling.
+            }
+            PieceType::Queen => {}
+            PieceType::Rook => {}
+            PieceType::Bishop => {}
+            PieceType::Knight => {}
+            PieceType::Pawn => {}
+            PieceType::None => {
+                return moves;
+            }
+        }
+
+        return moves;
     }
 }
 
@@ -117,6 +216,9 @@ impl Game {
                     _ => panic!("Unexpaced piece letter {}", c),
                 };
 
+                piece.x = x_pos;
+                piece.y = y_pos;
+
                 x_pos += 1;
             }
 
@@ -143,10 +245,50 @@ impl Game {
             panic!("Unexpected character for whose turn it is: {}. Should be 'w' or 'b'.", whose_turn);
         }
     }
+
+    fn print_all_legal_moves(&self) {
+        println!("Called print all legal moves!");
+
+        // Get all pieces to look at.
+        let mut our_pieces: Vec<&Piece> = vec![];
+        for row in self.board.iter() {
+            for piece in row.iter() {
+                if piece.piece_type != PieceType::None && piece.white == self.white_to_move {
+                    our_pieces.push(&piece);
+                }
+            }
+        }
+
+        /*
+            There is likely a better way of doing this. But just getting something on the screen.
+
+            To figure out what legal moves a piece has, we need the following
+            1. Which color the piece is.
+            2. The piece type.
+            3. Are we in check?
+            4. Is the piece pinned?
+            5. Castling rules?
+            6. En-Pessant rules?
+        */
+        for piece in our_pieces.iter() {
+            print!("Looking at moves for {}: ", piece.short_name);
+            let moves: Vec<Move> = piece.get_legal_moves(self);
+            for m in moves.iter() {
+                //print!("{:#?}", m); // debug pretty
+                //print!("{:?}", m); // debug
+                print!("{} ", m); // normal
+            }
+            println!();
+        }
+
+    }
 }
 
 fn main() {
     let mut game = Game::default();
-    game.import_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    //game.import_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    game.import_fen("rnbqkbnr/8/8/8/8/8/8/RNBQKBNR w KQkq - 0 1");
+    //game.import_fen("rnbqkbnr/8/8/8/8/8/8/RNBQKBNR b KQkq - 0 1");
     game.print_board();
+    game.print_all_legal_moves();
 }
