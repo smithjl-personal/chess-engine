1f2b16c090e33c59bfccc7d0606dd2fa99ca081f
master14270 <jessie.smith14270@gmail.com>
2024-12-26 00:18:27 -0500

    feat: Improvements to the bitboard!!!

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -16,14 +16,166 @@ impl Color {
     }
 }
 
+struct MagicNumberHelper {
+    pub state: u32,
+}
+
+impl MagicNumberHelper {
+    pub fn new() -> Self {
+        return MagicNumberHelper {
+            state: 1804289383, // Seed for our Psuedo-RNG.
+        }
+    }
+
+    fn get_random_number_u32(&mut self) -> u32 {
+        // Get current state. This is our seed.
+        let mut n = self.state;
+
+        // XOR Shift Algorithm to get a random number.
+        n ^= n << 13;
+        n ^= n >> 17;
+        n ^= n << 5;
+
+        // Update the state.
+        self.state = n;
+
+        return n;
+    }
+
+    fn get_random_number_u64(&mut self) -> u64 {
+        // Define some random numbers. We want the 16 bits from MSB1 side.
+        let n1: u64 = (self.get_random_number_u32()) as u64 & 0xFFFF;
+        let n2: u64 = (self.get_random_number_u32()) as u64 & 0xFFFF;
+        let n3: u64 = (self.get_random_number_u32()) as u64 & 0xFFFF;
+        let n4: u64 = (self.get_random_number_u32()) as u64 & 0xFFFF;
+
+        // Return them with fanciness.
+        return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48);
+    }
+
+    fn get_magic_number(&mut self) -> u64 {
+        return self.get_random_number_u64() & self.get_random_number_u64() & self.get_random_number_u64();
+    }
+
+    // Magic numbers?
+    fn find_magic_number(&mut self, square: u64, relevant_bits: usize, is_bishop: bool) -> u64 {
+
+        // Init occupancies, attack tables, and used attacks.
+        let mut occupancies: [u64; 4096] = [0; 4096];
+        let mut attacks: [u64; 4096] = [0; 4096];
+        let mut used_attacks: [u64; 4096];
+
+        // Init attack mask, either bishop or rook.
+        let attack_mask: u64;
+        if is_bishop {
+            attack_mask = mask_bishop_attacks(square);
+        } else {
+            attack_mask = mask_rook_attacks(square);
+        }
+
+        // Init occupancy indicies.
+        let occupancy_indicies: usize = 1 << relevant_bits;
+
+        // Loop over occupancy indicies.
+        for index in 0..occupancy_indicies {
+            occupancies[index] = set_occupancies(index, relevant_bits, attack_mask);
+
+            if is_bishop {
+                attacks[index] = dynamic_bishop_attacks(square, occupancies[index]);
+            } else {
+                attacks[index] = dynamic_rook_attacks(square, occupancies[index]);
+            }
+        }
+
+        // Now test for magic numbers. Should not take too long to run though!
+        for _ in 0..1_000_000_000 {
+            // Generate magic number candidate.
+            let magic_number = self.get_magic_number();
+
+            // This should be safe from overflow?
+            let (temp, _) = attack_mask.overflowing_mul(magic_number);
+            let to_check = temp & 0xFF00000000000000;
+
+            // Go next if we don't have enough bits.
+            if count_bits(to_check) < 6 {
+                continue;
+            }
+
+            // Clear out any used attacks from previous iteration.
+            used_attacks = [0; 4096];
+            let mut has_failed: bool = false;
+            for index in 0..occupancy_indicies {
+
+                // Overflow safe?
+                let (temp, _) = occupancies[index].overflowing_mul(magic_number);
+                let magic_index = (temp >> (64 - relevant_bits)) as usize;
+
+                if used_attacks[magic_index] == 0 {
+                    used_attacks[magic_index] = attacks[index];
+                } else if used_attacks[magic_index] != attacks[index] {
+                    has_failed = true;
+                    break;
+                }
+            }
+
+            if !has_failed {
+                return magic_number;
+            }
+        }
+
+        panic!("Unable to find magic number, oh no!");
+    }
+
+    // This function will print all the magic numbers, then they can be copied for later use in other parts of the program.
+    pub fn init_magic_numbers(&mut self, mut bishop_magic_numbers: [u64; 64], mut rook_magic_numbers: [u64; 64]) {
+        let bishop_relevant_bits: [usize; 64] = [
+            6, 5, 5, 5, 5, 5, 5, 6,
+            5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 7, 7, 7, 7, 5, 5,
+            5, 5, 7, 9, 9, 7, 5, 5,
+            5, 5, 7, 9, 9, 7, 5, 5,
+            5, 5, 7, 7, 7, 7, 5, 5,
+            5, 5, 5, 5, 5, 5, 5, 5,
+            6, 5, 5, 5, 5, 5, 5, 6,
+        ];
+
+        let rook_relevant_bits: [usize; 64] = [
+            12, 11, 11, 11, 11, 11, 11, 12,
+            11, 10, 10, 10, 10, 10, 10, 11,
+            11, 10, 10, 10, 10, 10, 10, 11,
+            11, 10, 10, 10, 10, 10, 10, 11,
+            11, 10, 10, 10, 10, 10, 10, 11,
+            11, 10, 10, 10, 10, 10, 10, 11,
+            11, 10, 10, 10, 10, 10, 10, 11,
+            12, 11, 11, 11, 11, 11, 11, 12,
+        ];
+
+        // For each square on the board.
+        for square in 0..64 {
+            // Handle rooks.
+            let n = self.find_magic_number(square, rook_relevant_bits[square as usize], false);
+            println!("0x{:X}ULL", n);
+            rook_magic_numbers[square as usize] = n;
+        }
+
+        println!("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n");
+
+        // For each square on the board.
+        for square in 0..64 {
+            // Handle bishops.
+            let n = self.find_magic_number(square, bishop_relevant_bits[square as usize], true);
+            println!("0x{:X}ULL", n);
+            bishop_magic_numbers[square as usize] = n;
+        }
+    }
+}
+
 pub const NOT_FILE_A: u64 = 18374403900871474942;
 pub const NOT_FILE_B: u64 = 18302063728033398269;
 pub const NOT_FILE_AB: u64 = 18229723555195321596;
 pub const NOT_FILE_G: u64 = 13816973012072644543;
 pub const NOT_FILE_H: u64 = 9187201950435737471;
 pub const NOT_FILE_GH: u64 = 4557430888798830399;
-
-//4557430888798830399
 
 pub const NOT_RANK_8: u64 = 18446744073709551360;
 pub const NOT_RANK_7: u64 = 18446744073709486335;
@@ -39,7 +191,9 @@ pub struct Constants {
     pub pawn_attacks: [[u64; 64]; 2],
     pub knight_attacks: [u64; 64],
     pub king_attacks: [u64; 64],
-    pub bishop_attacks: [u64; 64],
+
+    pub bishop_magic_numbers: [u64; 64],
+    pub rook_magic_numbers: [u64; 64],
 }
 
 impl Constants {
@@ -47,7 +201,9 @@ impl Constants {
         let mut pawn_attacks: [[u64; 64]; 2] = [[0; 64]; 2];
         let mut knight_attacks: [u64; 64] = [0; 64];
         let mut king_attacks: [u64; 64] = [0; 64];
-        let mut bishop_attacks: [u64; 64] = [0; 64];
+
+        let mut bishop_magic_numbers: [u64; 64] = [0; 64];
+        let mut rook_magic_numbers: [u64; 64] = [0; 64];
 
 
         for square in 0..64 {
@@ -56,14 +212,18 @@ impl Constants {
 
             knight_attacks[square] = mask_knight_attacks(square as u64);
             king_attacks[square] = mask_king_attacks(square as u64);
-            bishop_attacks[square] = mask_bishop_attacks(square as u64);
         }
 
+        let mut magic = MagicNumberHelper::new();
+        magic.init_magic_numbers(bishop_magic_numbers, rook_magic_numbers);
+        
+
         return Constants {
-            pawn_attacks: pawn_attacks,
-            knight_attacks: knight_attacks,
-            king_attacks: king_attacks,
-            bishop_attacks: bishop_attacks,
+            pawn_attacks,
+            knight_attacks,
+            king_attacks,
+            bishop_magic_numbers,
+            rook_magic_numbers,
         };
     }
 }
@@ -401,6 +561,90 @@ pub fn mask_rook_attacks(square: u64) -> u64 {
 }
 
 
+// Function a bit different than the others, it doesn't actually generate all the attacks...
+pub fn dynamic_rook_attacks(square: u64, block: u64) -> u64 {
+    let mut attacks: u64 = 0;
+
+    let target_rank: u64 = square / 8;
+    let target_file: u64 = square % 8;
+
+    let mut rank: u64;
+    let mut file: u64;
+
+    // Right.
+    rank = target_rank;
+    file = target_file + 1;
+    while file <= 7 {
+        let focus_square: u64 = 1 << (rank * 8 + file);
+        attacks |= focus_square;
+
+        // Exit if we are blocked.
+        if (focus_square & block) != 0 {
+            break;
+        }
+
+        file += 1;
+    }
+
+    // Left.
+    rank = target_rank;
+    file = match target_file.checked_sub(1) {
+        Some(f) => f,
+        None => 0,
+    };
+    loop {
+        let focus_square: u64 = 1 << (rank * 8 + file);
+        attacks |= focus_square;
+
+        // Exit if we are blocked.
+        if (focus_square & block) != 0 {
+            break;
+        }
+
+        file = match file.checked_sub(1) {
+            Some(f) => f,
+            None => break,
+        };
+    }
+
+    // Up.
+    rank = match target_rank.checked_sub(1) {
+        Some(r) => r,
+        None => 0,
+    };
+    file = target_file;
+    loop {
+        let focus_square: u64 = 1 << (rank * 8 + file);
+        attacks |= focus_square;
+
+        // Exit if we are blocked.
+        if (focus_square & block) != 0 {
+            break;
+        }
+
+        rank = match rank.checked_sub(1) {
+            Some(r) => r,
+            None => break,
+        };
+    }
+
+    // Down.
+    rank = target_rank + 1;
+    file = target_file;
+    while rank <= 7 {
+        let focus_square: u64 = 1 << (rank * 8 + file);
+        attacks |= focus_square;
+
+        // Exit if we are blocked.
+        if (focus_square & block) != 0 {
+            break;
+        }
+
+        rank += 1;
+    }
+
+    return attacks;
+}
 
 
 // Should this be an enum?
@@ -431,7 +675,34 @@ pub fn str_coord_to_bitboard_pos(s: &str) -> Result<usize, String> {
 }
 
 
+// I don't understand how this works yet...
+pub fn set_occupancies(index: usize, bits_in_mask: usize, mut attack_mask: u64) -> u64 {
+    let mut occupancy: u64 = 0;
 
+    // Loop over bit range in attack mask.
+    for count in 0..bits_in_mask {
+
+        // Get LSB of attack mask.
+        let square = match get_lsb_index(attack_mask) {
+            Ok(v) => v,
+            Err(m) => {
+                // For now, just panic. This shouldn't happen here?
+                panic!("{}", m);
+            }
+        };
+
+        // Pop the bit.
+        attack_mask = pop_bit(attack_mask, square as u64);
+
+        // Make sure occupancy is on the board.
+        if index & (1 << count) != 0 {
+            occupancy |= 1 << square;
+        }
+    }
+
+    return occupancy;
+
+}
 
 
 // Should these be macros? Or something similar?
@@ -449,4 +720,32 @@ pub fn pop_bit(bitboard: u64, square: u64) -> u64 {
     } else {
         return bitboard;
     }
+}
+
+pub fn count_bits(mut bitboard: u64) -> usize {
+    let mut bit_count = 0;
+
+    while bitboard != 0 {
+        bit_count += 1;
+
+        // Reset the least significant bit, once per iteration until there are no active bits left.
+        bitboard &= bitboard - 1;
+    }
+
+    return bit_count;
+}
+
+pub fn get_lsb_index(bitboard: u64) -> Result<usize, String> {
+    // Below operations will not work on bitboard of `0`.
+    if bitboard == 0 {
+        return Err("Illegal index requested.".to_string());
+    }
+
+    // Get the position of the least-significant bit, using some bit magic!
+    let lsb = bitboard & !bitboard + 1;
+
+    // Subtract `1` to populate the trailing bits.
+    let populated = lsb - 1;
+
+    return Ok(count_bits(populated));
 }
--- src/main.rs
+++ src/main.rs
@@ -26,39 +26,47 @@ async fn main() {
     //     bitboard::NOT_FILE_AB
     // );
 
-    let c = bitboard::Constants::new();
+    let mut c = bitboard::Constants::new();
 
     // Get a coord to test.
-    let mut bb: u64 = 0; // No squares.
-    let test_coord = bitboard::str_coord_to_bitboard_pos("d4").unwrap();
-    bb = bitboard::set_bit(bb, test_coord as u64);
-    println!("Piece located at:");
-    bitboard::print_bitboard(bb);
+    // let mut bb: u64 = 0; // No squares.
+    // let test_coord = bitboard::str_coord_to_bitboard_pos("d4").unwrap();
+    // bb = bitboard::set_bit(bb, test_coord as u64);
+    // println!("Piece located at:");
+    // bitboard::print_bitboard(bb);
 
     let mut blocker_bb: u64 = 0;
-    blocker_bb = bitboard::set_bit(
-        blocker_bb,
-        bitboard::str_coord_to_bitboard_pos("f2").unwrap() as u64
-    );
-    blocker_bb = bitboard::set_bit(
-        blocker_bb,
-        bitboard::str_coord_to_bitboard_pos("e5").unwrap() as u64
-    );
-    blocker_bb = bitboard::set_bit(
-        blocker_bb,
-        bitboard::str_coord_to_bitboard_pos("b6").unwrap() as u64
-    );
-    blocker_bb = bitboard::set_bit(
-        blocker_bb,
-        bitboard::str_coord_to_bitboard_pos("b2").unwrap() as u64
-    );
+    // blocker_bb = bitboard::set_bit(
+    //     blocker_bb,
+    //     bitboard::str_coord_to_bitboard_pos("d7").unwrap() as u64
+    // );
+    // blocker_bb = bitboard::set_bit(
+    //     blocker_bb,
+    //     bitboard::str_coord_to_bitboard_pos("d2").unwrap() as u64
+    // );
+    // blocker_bb = bitboard::set_bit(
+    //     blocker_bb,
+    //     bitboard::str_coord_to_bitboard_pos("b4").unwrap() as u64
+    // );
+    // blocker_bb = bitboard::set_bit(
+    //     blocker_bb,
+    //     bitboard::str_coord_to_bitboard_pos("g4").unwrap() as u64
+    // );
 
-    println!("Blocker bitboard:");
-    bitboard::print_bitboard(blocker_bb);
+    // let bit_count = bitboard::count_bits(blocker_bb);
+    // println!("Blocker bitboard (has {bit_count} blockers)");
+    // bitboard::print_bitboard(blocker_bb);
 
-    let piece_attacks = bitboard::dynamic_bishop_attacks(test_coord as u64, blocker_bb);
-    println!("Piece attacking:");
-    bitboard::print_bitboard(piece_attacks);
+    // other tests...
+    //let test_bb = blocker_bb & !blocker_bb + 1;
+    // println!("Index of LSB: {}", bitboard::get_lsb_index(blocker_bb).unwrap());
+    //bitboard::print_bitboard(get_lsb_index(blocker_bb));
+
+
+
+    // let piece_attacks = bitboard::dynamic_rook_attacks(test_coord as u64, blocker_bb);
+    // println!("Piece attacking:");
+    // bitboard::print_bitboard(piece_attacks);
 
     // Bitboard for all squares: 18446744073709551615
     //bitboard::print_bitboard(18446744073709551615);
@@ -67,4 +75,17 @@ async fn main() {
     //         bitboard::mask_rook_attacks(tile as u64)
     //     );
     // }
+
+    // Make an attack mask.
+    // let mask = bitboard::mask_rook_attacks(
+    //     str_coord_to_bitboard_pos("a1").unwrap() as u64
+    // );
+    // let occupancy = bitboard::set_occupancies(2, count_bits(mask), mask);
+    // print_bitboard(occupancy);
+
+
+    // bitboard::print_bitboard(
+    //     get_magic_number(&mut c)
+    // );
+    //bitboard::init_magic_numbers(&mut c);
 }
