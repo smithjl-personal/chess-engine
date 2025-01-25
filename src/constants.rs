579f8f9e6793a1d6dd85c7d1cfeb9fb5e1426424
master14270 <jessie.smith14270@gmail.com>
2025-01-25 09:39:00 -0500

    chore: Move all old approach code into old folder.

--- src/constants.rs
+++ src/constants.rs
@@ -1,24 +1,4 @@
-use crate::piece_type::PieceType;
-
-// Original Implementation.
-pub const BOARD_SIZE: usize = 8;
-pub const KNIGHT_MOVES: [(i16, i16); 8] = [
-    (-2, -1),
-    (-2, 1),
-    (-1, 2),
-    (1, 2),
-    (2, 1),
-    (2, -1),
-    (1, -2),
-    (-1, -2),
-];
 pub const INITIAL_GAME_STATE_FEN: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-pub const PAWN_PROMOTION_CHOICES: [PieceType; 4] = [
-    PieceType::Queen,
-    PieceType::Rook,
-    PieceType::Knight,
-    PieceType::Bishop,
-];
 pub const LICHESS_BOT_USERNAME: &str = "botmasterj";
 pub const LICHESS_CHALLENGER_WHITELIST: [&str; 1] = ["The_Big_C"];
 
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
 pub mod bitboard;
-pub mod castle_sides;
 pub mod constants;
-pub mod coord;
-pub mod g; // Something is wrong with rust-analyzer. This is the only way it will pick up the changes right now.
-pub mod game_states;
 pub mod lichess;
 pub mod lichess_structs;
-pub mod my_move;
-pub mod piece;
-pub mod piece_type;
-pub mod tests;
 pub mod runtime_calculated_constants;
 
 #[tokio::main]
